b53a6d3220840f284330d84611b6286d48ef9083
Flavio Vicentini <flaviovicentini1991@gmail.com>
2023-09-19 10:34:50 -0300

    First commit

---
+++ src/main/java/br/com/flaviovicentini/LessonJavaWeb/LessonJavaWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.flaviovicentini.LessonJavaWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonJavaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonJavaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/flaviovicentini/LessonJavaWeb/LessonJavaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.flaviovicentini.LessonJavaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonJavaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
